67b12c55ac7fce167598f9812e31660ea987c1b0
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-17 21:36:39 +0330

    artistserv class has written

---
+++ artistservice.cpp
@@ -0,0 +1,21 @@
+#include "artistservice.h"
+
+ArtistService::ArtistService(AlbumRepository& arep,SongRepository& srep):albumrep(arep) , songrep(srep){};
+int ArtistService::createAlbum(int artistid,string& name){
+    Album album(name, 0,artistid);
+    return albumrep.save(album);
+};
+void ArtistService::updateAlbum(Album& album){albumrep.save(album);};
+void ArtistService::deleteAlbum(int albumid){albumrep.remove(albumid);};
+int ArtistService::createSong(int artistid, string& name, int year,string& genre,string& filepath,int albumid=0){
+    Song song(name,year,genre,filepath,0,artistid,albumid);
+    return songrep.save(song);
+};
+void ArtistService::updateSong(Song& song){songrep.save(song);};
+void ArtistService::deleteSong(int songid){songrep.remove(songid);};
+vector<Album> ArtistService::getArtistAlbum(int artistid){return albumrep.albums(artistid);};
+vector<Song> ArtistService::getAlbumSong(int albumid){return songrep.getByAlbum(albumid);};
+vector<Song> ArtistService::getSingle(int artistid){return songrep.singleSongs(artistid);};
+
+
+
---
+++ artistservice.h
@@ -0,0 +1,24 @@
+#ifndef ARTISTSERVICE_H
+#define ARTISTSERVICE_H
+#pragma once
+#include "AlbumRepository.h"
+#include "SongRepository.h"
+
+class ArtistService
+{
+    AlbumRepository& albumrep;
+    SongRepository& songrep;
+public:
+    ArtistService(AlbumRepository& arep,SongRepository& srep);
+    int createAlbum(int artistid,string& name);
+    void updateAlbum(Album& album);
+    void deleteAlbum(int albumid);
+    int createSong(int artistid, string& name, int year,string& genre,string& filepath,int albumid=0);
+    void updateSong(Song& song);
+    void deleteSong(int songid);
+    vector<Album> getArtistAlbum(int artistid);
+    vector<Song> getAlbumSong(int albumid);
+    vector<Song> getSingle(int artistid);
+};
+
+#endif // ARTISTSERVICE_H

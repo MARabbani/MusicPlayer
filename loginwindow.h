9b677f6715bd14064069ccc9b2bd4dfeba37a465
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-19 01:33:13 +0330

    loginwin completed

---
+++ loginwindow.h
@@ -0,0 +1,37 @@
+#ifndef LOGINWINDOW_H
+#define LOGINWINDOW_H
+#pragma once
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include "AuthService.h"
+#include <QString>
+class LoginWindow:public QWidget
+{
+    Q_OBJECT
+    QLineEdit* usernameedit;
+    QLineEdit* passwordedit;
+    QPushButton* loginbtn;
+    QPushButton* gotosignupbtn;
+    QLabel* errorlabel;
+    AuthService& authservice;
+public:
+    LoginWindow(AuthService& service,QWidget* parent=nullptr): QWidget(parent),authservice(service){};
+signals:
+    void loginSuccess(Account acc);
+    void switchToSignUp();
+private slots:
+    void onLoginClicked(){
+        try{
+            string user=usernameedit->text().toStdString();
+            string pass=passwordedit->text().toStdString();
+            Account acc = authservice.login(user,pass);
+            emit loginSuccess(acc);
+        } catch (exception& e){
+            errorlabel->setText(QString::fromStdString(e.what()));
+        }
+    }
+};
+
+#endif // LOGINWINDOW_H

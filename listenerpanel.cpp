a1b40edfd93cb8c23e496ade40019dd2e9a3d38f
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-20 02:42:05 +0330

    listenerpanel completed

---
+++ listenerpanel.cpp
@@ -0,0 +1,3 @@
+#include "listenerpanel.h"
+
+ListenerPanel::ListenerPanel() {}
---
+++ listenerpanel.h
@@ -0,0 +1,29 @@
+#ifndef LISTENERPANEL_H
+#define LISTENERPANEL_H
+#pragma once
+#include <QWidget>
+#include <QListWidget>
+#include <QLineEdit>
+#include "ListenerService.h"
+
+class ListenerPanel:public QWidget{
+    Q_OBJECT
+private:
+    Account currentlistener;
+    ListenerService& listenerservice;
+    QListWidget* playlistlist;
+    QListWidget* songlist;
+    QLineEdit* searchedit;
+
+public:
+    ListenerPanel(Account listener , ListenerService& service, QWidget* parent=nullptr);
+private slots:
+    void refreshPlaylists() {
+        playlistlist->clear();
+        playlistlist->addItem("Songs Favorite"); // ثابت
+        for (auto& p:listenerservice.getPlaylists(currentlistener.id))
+            playlistlist->addItem(QString::fromStdString(p.playlistname));
+    }
+};
+
+#endif // LISTENERPANEL_H

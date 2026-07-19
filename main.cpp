70208d501d79ac01ab6f8bf5bb06a58db37e8fc9
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-20 03:24:22 +0330

    main debuging

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-// #include "mainwindow.h"
+#include <QApplication>
+#include "mainwindow.h"
+#include "artistservice.h"
+#include "listenerservice.h"
 
-// #include <QApplication>
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
 
-// int main(int argc, char *argv[])
-// {
-//     QApplication a(argc, argv);
-//     MainWindow w;
-//     w.show();
-//     return QApplication::exec();
-// }
+    AccountRepository accountrep;
+    AlbumRepository albumrep;
+    SongRepository songrep;
+    PlaylistRepository playlistrep;
+    ListenerRepository listenerrep;
+
+    AuthService authService(accountrep);
+    ArtistService artistService(albumrep,songrep);
+    ListenerService listenerService(playlistrep,songrep,listenerrep);
+
+    MainWindow w(authService,artistService,listenerService);
+    w.show();
+
+    return a.exec();
+}

b8e6657a0cd7faf5fde389211912d8288346bdb9
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-20 04:34:43 +0330

    artistpanel widget

--- artistpanel.cpp
+++ artistpanel.cpp
@@ -1,6 +1,24 @@
 #include "artistpanel.h"
+#include <QVBoxLayout>
 ArtistPanel::ArtistPanel(Account artist, ArtistService& service, QWidget* parent):QWidget(parent),currentartist(artist),artistservice(service)
 {
+    albumlist = new QListWidget(this);
+    songlist  = new QListWidget(this);
+    addalbumbtn = new QPushButton("Add Album", this);
+    addsongbtn  = new QPushButton("Add Song", this);
+    editbtn     = new QPushButton("Edit", this);
+    deletebtn   = new QPushButton("Delete", this);
+
+    auto* layout = new QVBoxLayout(this);
+    layout->addWidget(albumlist);
+    layout->addWidget(addalbumbtn);
+    layout->addWidget(songlist);
+    layout->addWidget(addsongbtn);
+    layout->addWidget(editbtn);
+    layout->addWidget(deletebtn);
+
+    connect(albumlist, &QListWidget::currentRowChanged, this, &ArtistPanel::refreshSongs);
+    refreshAlbums();
 }
 void ArtistPanel::refreshAlbums() {
     albumlist->clear();

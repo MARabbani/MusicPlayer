4921b28d124a35c3f05952b429dd63a973e7f959
Mohammad <ma.rabbani.kh@gmail.com>
2026-07-20 03:58:01 +0330

    signupwin debug

--- signupwindow.cpp
+++ signupwindow.cpp
@@ -1,2 +1,26 @@
 #include "signupwindow.h"
+#include <string>
+#include <QString>
 
+using namespace std;
+
+SignUpWindow::SignUpWindow(AuthService& service,QWidget* parent)
+    : QWidget(parent), authservice(service)
+{}
+
+void SignUpWindow::onSignUpClicked(){
+    try {
+        string name = fullnameedit->text().toStdString();
+        string user = usernameedit->text().toStdString();
+        string pass = passwordedit->text().toStdString();
+        Role role = rolebox->currentText() == "Artist" ? Role::Artist : Role::Listener;
+        string bio = bioedit->text().toStdString();
+
+        Account newaccount(0, name, user, bio, role, pass);
+        int newaccid = authservice.signUp(newaccount);
+
+        emit signUpSuccess(newaccount);
+    } catch (const exception& e) {
+        errorLabel->setText(QString::fromStdString(e.what()));
+    }
+}
